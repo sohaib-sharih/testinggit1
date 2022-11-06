d83c0f8ce5db742a36574fd0e661290efdc59d69
Sohaib Sharih <sohaib.sharih@gmail.com>
2022-11-07 02:53:27 +0500

    first commit

---
+++ test.js
@@ -0,0 +1,9 @@
+var a = 3;
+var b = 3;
+var simple = a + b;
+console.log(`this is the sum of simple => ${simple}`)
+
+const y = 4
+const x = 3
+const sum = x + y
+console.log(`This is the total of sum ${sum}`)
