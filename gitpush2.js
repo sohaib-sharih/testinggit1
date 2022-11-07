7545458db2d46dea8e21aed3c4975937484ffa37
sohaib <sohaib.sharih@gmail.com>
2022-11-08 04:15:35 +0500

    nine

--- gitpush2.js
+++ gitpush2.js
@@ -1,2 +1,11 @@
 // hello
 // hi
+const person = {
+    firstname: "sohaib",
+    lastname: "sharih",
+    id: 500,
+    fullname: function(){
+        console.log(this.firstname  + " " + this.lastname)
+    }
+}
+console.log(person.fullname())
