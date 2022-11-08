06af91c1c3f8669e5e41ad92c60cb1450019c02e
sohaib <sohaib.sharih@gmail.com>
2022-11-08 05:09:19 +0500

    ten

--- gitpush2.js
+++ gitpush2.js
@@ -4,8 +4,11 @@ const person = {
     firstname: "sohaib",
     lastname: "sharih",
     id: 500,
-    fullname: function(){
-        console.log(this.firstname  + " " + this.lastname)
+    fullname: function(babe){
+        console.log(this.firstname  + babe +" " + this.lastname)
+        // return this.firstname  + babe +" " + this.lastname
     }
 }
-console.log(person.fullname())
+
+// console.log(person.fullname(50)) // This will give undefined because the value is not being stored in another variable. 
+var getFull = person.fullname(500)
